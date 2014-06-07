1aa6b4c946bf77577577c065839e658479e33ffd
lmdddolat <lmdddolat@gmail.com>
2014-06-07 04:59:01 -0700

    test project
    test project

---
+++ abstartctFactory/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstartctFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+   public interface  VehicleFactort {
+       bike Createbike();
+       Scooter CreateScooter();
+
+}
+
+    public abstract class HondaBike:VehicleFactort{
+
+        public bike Createbike()
+        {
+            return new bike();
+           
+
+
+
+        }
+
+        public Scooter CreateScooter()
+        {
+            return new Scooter();
+        }
+
+
+      
+    }
+
+
+}
